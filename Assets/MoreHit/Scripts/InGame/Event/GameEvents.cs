62e30fa7673ccf3ec0af28640c0e711ff1a3b556
YutoImata <yuto.imata.wine@gmail.com>
2025-12-23 20:45:34 +0900

    ゲームのイベントクラスを静的に定義、作成

---
+++ Assets/MoreHit/Scripts/InGame/Event/GameEvents.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace MoreHit.Events
+{
+    /// <summary>
+    /// ゲーム全体のイベントを管理する静的クラス
+    /// 各システムがここに定義されたイベントを購読・発火する
+    /// </summary>
+    public static class GameEvents
+    {
+        /// <summary>
+        /// プレイヤーが死亡した時
+        /// </summary>
+        public static event Action OnPlayerDeath;
+        
+        public static void TriggerPlayerDeath()
+        {
+            OnPlayerDeath?.Invoke();
+        }
+        
+        /// <summary>
+        /// 全てのイベントリスナーをクリアする（シーン切り替え時などに使用）
+        /// </summary>
+        public static void ClearAllEvents()
+        {
+            OnPlayerDeath = null;
+        }
+    }
+}
