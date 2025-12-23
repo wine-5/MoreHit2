854d0b9e061e8d2e381d163201fa265b2726e064
YutoImata <yuto.imata.wine@gmail.com>
2025-12-23 19:43:53 +0900

    タイトルにボタンを追加し、シーン遷移できるようにTitleSceneを設定

--- Assets/MoreHit/Scripts/Scene/SceneChangeButtonHandler.cs
+++ Assets/MoreHit/Scripts/Scene/SceneChangeButtonHandler.cs
@@ -21,44 +21,5 @@ namespace MoreHit.Scene
             else
                 Debug.LogWarning("SceneController instance not found!");
         }
-
-        /// <summary>
-        /// ButtonのOnClickから呼び出されるメソッド
-        /// Titleシーンに切り替える
-        /// </summary>
-        public void GoToTitle()
-        {
-            ChangeSceneTo(SceneName.Title);
-        }
-
-        /// <summary>
-        /// ButtonのOnClickから呼び出されるメソッド
-        /// InGameシーンに切り替える
-        /// </summary>
-        public void GoToInGame()
-        {
-            ChangeSceneTo(SceneName.InGame);
-        }
-
-        /// <summary>
-        /// ButtonのOnClickから呼び出されるメソッド
-        /// Resultシーンに切り替える
-        /// </summary>
-        public void GoToResult()
-        {
-            ChangeSceneTo(SceneName.Result);
-        }
-
-        /// <summary>
-        /// 指定されたシーンに切り替える
-        /// </summary>
-        /// <param name="sceneName">遷移先のシーン</param>
-        private void ChangeSceneTo(SceneName sceneName)
-        {
-            if (SceneController.I != null)
-                SceneController.I.LoadScene(sceneName);
-            else
-                Debug.LogWarning("SceneController instance not found!");
-        }
     }
 }
