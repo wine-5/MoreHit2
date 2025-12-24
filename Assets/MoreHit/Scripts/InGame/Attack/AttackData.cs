4f1a807277dfd7588798ca7df47c30f0baf4ed8f
YutoImata <yuto.imata.wine@gmail.com>
2025-12-24 11:44:49 +0900

    AttackDataクラスを作成し必要な攻撃の譲歩を登録できるように処理を追加

---
+++ Assets/MoreHit/Scripts/InGame/Attack/AttackData.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace MoreHit.Attack
+{
+    [CreateAssetMenu(fileName = "AttackData", menuName = "MoreHit/AttackData")]
+    public class AttackData : ScriptableObject
+    {
+        [Header("基本パラメータ")]
+        [SerializeField] private float damage = 10f;
+        [SerializeField] private float range = 1.5f;
+        [SerializeField] private Vector2 hitboxSize = new Vector2(1f, 1f);
+        [SerializeField] private LayerMask targetLayers = -1;
+        
+        [Header("ストックシステム")]
+        [SerializeField] private int stockAmount = 1;
+        
+        [Header("エフェクト")]
+        [SerializeField] private GameObject hitEffectPrefab;
+        
+        // プロパティで公開
+        public float Damage => damage;
+        public float Range => range;
+        public Vector2 HitboxSize => hitboxSize;
+        public LayerMask TargetLayers => targetLayers;
+        public int StockAmount => stockAmount;
+        public GameObject HitEffectPrefab => hitEffectPrefab;
+    }
+}
