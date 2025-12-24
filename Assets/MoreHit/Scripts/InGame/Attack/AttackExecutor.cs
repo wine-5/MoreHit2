934e5d3424c21a6824384088f587264ae07504ea
YutoImata <yuto.imata.wine@gmail.com>
2025-12-24 13:51:08 +0900

    AttackExecutorにシングルトンの基底クラスを継承させた

--- Assets/MoreHit/Scripts/InGame/Attack/AttackExecutor.cs
+++ Assets/MoreHit/Scripts/InGame/Attack/AttackExecutor.cs
@@ -1,69 +1,102 @@
 using UnityEngine;
-using MoreHit.Enemy;
 
 namespace MoreHit.Attack
 {
-    public class AttackExecutor : MonoBehaviour
+    public class AttackExecutor : Singleton<AttackExecutor>
     {
-        public static AttackExecutor I { get; private set; }
+        private const float EFFECT_LIFETIME = 2f;
 
-        [Header("Debug")]
-        public bool showDebugGizmos = true;
+        protected override bool UseDontDestroyOnLoad => true;
 
-        void Awake()
-        {
-            if (I == null)
-                I = this;
-            else
-                Destroy(gameObject);
-        }
-
+        /// <summary>
+        /// 攻撃を実行し、ヒット数を返す
+        /// </summary>
         public int Execute(AttackData data, Vector3 origin, Vector2 direction, GameObject attacker)
         {
-            Vector3 hitPos = origin + (Vector3)direction * data.Range;
-            Collider2D[] hits = Physics2D.OverlapBoxAll(hitPos, data.HitboxSize, 0);
+            if (data == null || attacker == null)
+            {
+                Debug.LogWarning("AttackData or Attacker is null!");
+                return 0;
+            }
 
+            Vector3 hitPosition = CalculateHitPosition(origin, direction, data.Range);
+            Collider2D[] hits = DetectHits(hitPosition, data.HitboxSize);
+
+            return ProcessHits(hits, data, attacker);
+        }
+
+        private Vector3 CalculateHitPosition(Vector3 origin, Vector2 direction, float range)
+        {
+            return origin + (Vector3)direction * range;
+        }
+
+        private Collider2D[] DetectHits(Vector3 position, Vector2 size)
+        {
+            return Physics2D.OverlapBoxAll(position, size, 0f);
+        }
+
+        private int ProcessHits(Collider2D[] hits, AttackData data, GameObject attacker)
+        {
             int hitCount = 0;
+
             foreach (var hit in hits)
             {
-                if (hit.gameObject == attacker) continue;
+                if (ShouldIgnoreHit(hit, attacker, data.TargetTags))
+                    continue;
 
-                // タグチェック
-                bool isValidTarget = false;
-                foreach (string targetTag in data.TargetTags)
-                {
-                    if (hit.CompareTag(targetTag))
-                    {
-                        isValidTarget = true;
-                        break;
-                    }
-                }
-
-                if (!isValidTarget) continue;
-
-                // ダメージ適用
-                var damageable = hit.GetComponent<IDamageable>();
-                if (damageable != null)
-                {
-                    damageable.TakeDamage((int)data.Damage);
+                if (ApplyDamage(hit, data.Damage))
                     hitCount++;
-                }
 
-                // ストック適用 
-                var stockable = hit.GetComponent<IStockable>();
-                if (stockable != null && data.StockAmount > 0)
-                {
-                    stockable.AddStock(data.StockAmount);
-                }
+                ApplyStock(hit, data.StockAmount);
+                SpawnHitEffect(hit.transform.position, data.HitEffectPrefab);
+            }
 
-                // エフェクト生成
-                if (data.HitEffectPrefab != null)
-                {
-                    var effect = Instantiate(data.HitEffectPrefab, hit.transform.position, Quaternion.identity);
-                    Destroy(effect, 2f);
-                }
-            }            
             return hitCount;
+        }
+
+        private bool ShouldIgnoreHit(Collider2D hit, GameObject attacker, string[] targetTags)
+        {
+            if (hit.gameObject == attacker)
+                return true;
+
+            return !HasValidTag(hit, targetTags);
+        }
+
+        private bool HasValidTag(Collider2D hit, string[] targetTags)
+        {
+            foreach (string tag in targetTags)
+            {
+                if (hit.CompareTag(tag))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool ApplyDamage(Collider2D hit, int damage)
+        {
+            var damageable = hit.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.TakeDamage(damage);
+                return true;
+            }
+            return false;
+        }
+
+        private void ApplyStock(Collider2D hit, int stockAmount)
+        {
+            if (stockAmount <= 0) return;
+
+            var stockable = hit.GetComponent<IStockable>();
+            stockable?.AddStock(stockAmount);
+        }
+
+        private void SpawnHitEffect(Vector3 position, GameObject effectPrefab)
+        {
+            if (effectPrefab == null) return;
+
+            GameObject effect = Instantiate(effectPrefab, position, Quaternion.identity);
+            Destroy(effect, EFFECT_LIFETIME);
         }
     }
 }
