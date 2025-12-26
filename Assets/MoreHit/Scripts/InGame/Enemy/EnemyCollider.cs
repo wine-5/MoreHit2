a80e9923b469e7d1d281765b161d4fd353223f68
YutoImata <yuto.imata.wine@gmail.com>
2025-12-26 21:18:08 +0900

    敵が吹っ飛中のときは早期returnしてプレイヤーにダメージを与えないように修正

--- Assets/MoreHit/Scripts/InGame/Enemy/EnemyCollider.cs
+++ Assets/MoreHit/Scripts/InGame/Enemy/EnemyCollider.cs
@@ -23,8 +23,8 @@ namespace MoreHit
             // 衝突相手のタグをチェック
             if (collision.gameObject.CompareTag("Player"))
             {
-                // プレイヤーへ攻撃を実行
-                if (enemyBase != null && !enemyBase.IsDead)
+                // プレイヤーへ攻撃を実行（敵が死亡していないか、吹っ飛び状態でないかチェック）
+                if (enemyBase != null && !enemyBase.IsDead && !enemyBase.IsInLaunchState())
                     enemyBase.AttackPlayer();
             }
         }
